245a89d530aa123788fe205efe900aa9d88a24f6
madhawa-se <madhawapriyashantha@gmail.com>
2024-05-21 09:59:02 +0530

    fix cors issues

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,14 @@ import cors from 'cors';
 
 const app = express();
 const port = 1337;
+
+app.options('*', cors());
+app.use(cors());
+
 const httpServer = createServer(app);
 
 Logger.info("Running the app");
 
-app.options('*', cors());
 
 app.use("/api/v1", api());
 
